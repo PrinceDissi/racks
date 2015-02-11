c001cde1ca049492c97d4ec72bcfbacb42daf9f5
Jan Biasi <biasijan@gmail.com>
2015-02-11 20:43:07 +0100

    Update core files

--- dist/racks.js
+++ dist/racks.js
@@ -1,4 +1,8 @@
 /* global platform */
 (function() {
-    
+    window.addEventListener('HTMLImportsLoaded', function() {
+        Racks.__queue.forEach(function(handler) {
+            handler();
+        });
+    });
 })();
