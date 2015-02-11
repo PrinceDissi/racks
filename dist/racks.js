25b99cc3173c22ca6330d4429113b8ab05f853be
Jan Biasi <biasijan@gmail.com>
2015-02-11 21:37:08 +0100

    Add fix for lack in rendering in Firefox OS2

--- dist/racks.js
+++ dist/racks.js
@@ -1,10 +1,5 @@
 /* global platform */
 (function() {
-    document.body.style.opacity = 0;
-    window.addEventListener('WebComponentsReady', function() {
-        document.body.style.opacity = 1;
-    });
-
     // Call handlers for finished imports
     window.addEventListener('HTMLImportsLoaded', function() {
         Racks.__queue.forEach(function(handler) {
