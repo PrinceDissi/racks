10a376ee02bfbc516e76c2791893114a230e5b79
Jan R. Biasi <janbiasi@users.noreply.github.com>
2015-02-23 15:45:08 +0100

    Update index.js

--- create/index.js
+++ create/index.js
@@ -1,5 +1,10 @@
 #!/bin/env node
 
+var root = path.join(__dirname, '../');
+var templates = path.join(root, 'templates/');
+
+console.log(templates);
+
 process.argv.forEach(function(entry, index, arr) {
   console.log(entry);
 });
